fe448b83049398cc9dd5aceb3eeb4013e25222b4
ltghuy <letrangiahuy0507@gmail.com>
2022-11-01 11:22:53 +0700

    Add favicon

--- containers/AboutSection/index.tsx
+++ containers/AboutSection/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const AboutSection = () => {
-  return <div>About content here</div>
+  return (
+    <div className="grid grid-cols-2 gap-10 items-center">
+      <div className="col-span-1">Avatar here</div>
+      <div className="col-span-1">Content here</div>
+    </div>
+  )
 }
 
 export default AboutSection
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,44 @@
+import Head from 'next/head'
 import '../styles/color.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/favicon/favicon-512x512.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/favicon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
