1435fcb8e1b2ac3aedd174b1359ee7f4a7ea78f3
ltghuy <letrangiahuy0507@gmail.com>
2022-12-26 08:14:07 +0700

    Update CV

--- containers/SkillSection/index.tsx
+++ containers/SkillSection/index.tsx
@@ -30,33 +30,33 @@ const SkillSection = () => {
   const educationTimeline = [
     {
       id: 1,
+      time: '2021 - Present',
+      title: 'Self learning',
+      subTitle: 'Udemy, FreeCodeCamp',
+      desc: `<ul class='list-disc'><li>Learn about VueJS, TailwindCSS.</li><li>Apply the knowledge learned to build a few personal projects.</li><li>Improve language skills.</li></ul>`,
+    },
+    {
+      id: 2,
       time: '2017 - 2021',
       title: 'College graduation',
       subTitle: 'Can Tho university.',
       desc: `<ul class='list-disc'><li>Basic knowledge of programming, algorithms, computers.</li><li>System Analysis and Design.</li/><li>Building a programming mindset.</li><li>Get familiar with the database.</li></ul>`,
     },
-    {
-      id: 2,
-      time: '2021 - Current',
-      title: 'Self learning',
-      subTitle: 'Udemy, FreeCodeCamp',
-      desc: `<ul class='list-disc'><li>Learn about VueJS, TailwindCSS.</li><li>Apply the knowledge learned to build a few personal projects.</li><li>Improve language skills.</li></ul>`,
-    },
   ]
   const workingTimeline = [
     {
       id: 1,
+      time: '03/2022 - Present',
+      title: 'Front-end Developer',
+      subTitle: 'Skyblaze Studios.',
+      desc: `<ul class='list-disc'><li>Develop and maintain front-end functionality of websites.</li><li>Do multiple projects simultaneously with high standards and performance.</li/><li>Get involved in creating a website from scratch.</li><li>Participate in finding and fixing bugs to optimize project performance.</li></ul>`,
+    },
+    {
+      id: 2,
       time: '01/2022',
       title: 'Front-end Developer - Intern',
       subTitle: 'Skyblaze Studios.',
       desc: `<ul class='list-disc'><li>Learn about ReactJS framework, Typescript and Git.</li><li>Approach the workflow and participate in the construction of a few real projects.</li/></ul>`,
-    },
-    {
-      id: 2,
-      time: '03/2022 - Current',
-      title: 'Front-end Developer',
-      subTitle: 'Skyblaze Studios.',
-      desc: `<ul class='list-disc'><li>Develop and maintain front-end functionality of websites.</li><li>Do multiple projects simultaneously with high standards and performance.</li/><li>Get involved in creating a website from scratch.</li><li>Participate in finding and fixing bugs to optimize project performance.</li></ul>`,
     },
   ]
   return (
